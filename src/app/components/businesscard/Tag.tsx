60af2aa94d8ac40cbe33b45d01af33324d83ae91
wkzhiro <wkzhiro0116@yahoo.co.jp>
2025-03-15 23:00:34 +0900

    chanege tag border

--- src/app/components/businesscard/Tag.tsx
+++ src/app/components/businesscard/Tag.tsx
@@ -3,7 +3,7 @@ import { TagProps } from "@/types/businesscard";
 
 export const Tag: React.FC<TagProps> = ({ text }) => {
   return (
-    <div className="gap-1 self-stretch px-3 pt-0.5 pb-1 my-auto bg-red-100 rounded-xl text-xs">
+    <div className="gap-1 self-stretch px-3 pt-0.5 pb-1 my-auto border border-black p-2 rounded-xl text-xs">
       {text}
     </div>
   );
